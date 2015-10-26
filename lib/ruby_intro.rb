8bece5c3135a2de8608fc5e7fc2cf70124506884
Santiago Medina <devsanlizmed@yahoo.com>
2015-10-26 02:20:35 +0000

    Part 1 completed

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -4,14 +4,43 @@
 
 def sum arr
   # YOUR CODE HERE
+  result = 0
+  for num in arr do
+    result += num
+  end
+  return result
 end
 
 def max_2_sum arr
   # YOUR CODE HERE
+  maxNegative = -(1 << (0.size*32))
+  max1 = maxNegative
+  max2 = maxNegative
+  for num in arr do
+    if num > max1
+      max2 = max1
+      max1 = num
+    elsif num > max2
+      max2 = num
+    end
+  end
+  max1 = 0 if max1 == maxNegative
+  max2 = 0 if max2 == maxNegative
+  return max1 + max2
 end
 
 def sum_to_n? arr, n
   # YOUR CODE HERE
+  i = 0
+  while i < arr.size do
+    j = i + 1
+    while j < arr.size do
+      return true if arr[i] + arr[j] == n
+      j += 1
+    end
+    i += 1
+  end
+  return false
 end
 
 # Part 2
